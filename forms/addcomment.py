304bd0b271344315fddd8a1e08198432ab75bd08
kirilllllllllllllllllllllllllllll <150334314+kirilllllllllllllllllllllllllllll@users.noreply.github.com>
2025-03-19 21:00:06 +0700

    forms

--- forms/addcomment.py
+++ forms/addcomment.py
@@ -1,8 +1,8 @@
 from flask_wtf import FlaskForm
-from wtforms import TextAreaField, SubmitField
+from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 
 
 class AddComment(FlaskForm):
-    content = TextAreaField('Комментарий', validators=[DataRequired()])
+    content = StringField('Комментарий', validators=[DataRequired()])
     submit = SubmitField('Готово')
