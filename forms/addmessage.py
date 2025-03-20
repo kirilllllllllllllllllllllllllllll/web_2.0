ca194cae61839f2a9d38a3c616eb9ef8fc2daa18
Dmitriy <ytude7042@gmail.com>
2025-03-20 07:04:09 +0700

    Read_chat bug-fix

--- forms/addmessage.py
+++ forms/addmessage.py
@@ -1,9 +1,9 @@
 from flask_wtf import FlaskForm
-<<<<<<< HEAD
+# <<<<<<< HEAD
 from wtforms import TextAreaField, SubmitField, StringField
-=======
+# =======
 from wtforms import SubmitField, StringField
->>>>>>> 304bd0b271344315fddd8a1e08198432ab75bd08
+#>>>>>>> 304bd0b271344315fddd8a1e08198432ab75bd08
 from wtforms.validators import DataRequired
 
 
