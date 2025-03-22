ddd0ac98231de9623d41adc389f7a708f29b1e24
kirilllllllllllllllllllllllllllll <150334314+kirilllllllllllllllllllllllllllll@users.noreply.github.com>
2025-03-22 10:12:07 +0700

    delete_useless_things

--- forms/addmessage.py
+++ forms/addmessage.py
@@ -1,5 +1,9 @@
 from flask_wtf import FlaskForm
+# <<<<<<< HEAD
+from wtforms import TextAreaField, SubmitField, StringField
+# =======
 from wtforms import SubmitField, StringField
+#>>>>>>> 304bd0b271344315fddd8a1e08198432ab75bd08
 from wtforms.validators import DataRequired
 
 
--- main.py
+++ main.py
@@ -40,9 +40,9 @@ from PIL import Image
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'kirik1234pro_and_thisismyshadow_secret_key'
-app.config['UPLOAD_FOLDER'] = os.getcwd() + "\static\profile_photos"
-app.config['UPLOAD_FOLDER2'] = os.getcwd() + "\static\chat_imgs"
-app.config['UPLOAD_FOLDER3'] = os.getcwd() + "\static\posts"
+app.config['UPLOAD_FOLDER'] = os.getcwd() + "/static/profile_photos"
+app.config['UPLOAD_FOLDER2'] = os.getcwd() + "/static/chat_imgs"
+app.config['UPLOAD_FOLDER3'] = os.getcwd() + "/static/posts"
 SECRET_CODE1 = '124_9713'
 SECRET_CODE2 = '124_nexus'
 CONST = ['7А', '7Б', '7В', '7Г', '8А', '8Б', '8В', '8Г', '9А', '9Б', '9В', '9Г', '10А', '10Б', '10В', '10Г', '11А',
@@ -155,11 +155,9 @@ def reqister():
                         name=form.name.data,
                         email=form.email.data,
                         form=st,
-                        informal_name=form.informal_name.data,
-                        about=form.about.data,
                         publications='0',
                         friends='0',
-                        photo='watch_cat_2.png',
+                        photo='watch_cat_1.png',
                         potential_friends='0',
                         messages='0',
                         messages_bad='0',
@@ -187,8 +185,6 @@ def reqister():
                             name=form.name.data,
                             email=form.email.data,
                             form=st,
-                            informal_name=form.informal_name.data,
-                            about=form.about.data,
                             publications='0',
                             friends='0',
                             photo='0',
@@ -702,6 +698,7 @@ def read_private_chat(id):
             name=current_user.name,
             photo=current_user.photo,
             chat=id,
+            sticker='none',
             time=str(datetime.now()).split('.')[0][:-3].split()[1]
         )
         db_sess.add(message)
@@ -712,7 +709,6 @@ def read_private_chat(id):
     data = db_sess.query(Chat).filter(Chat.peoples.like(f'%{current_user.id}%')).all()
     data2 = db_sess.query(PrivateChat).filter(
         or_(PrivateChat.user2 == current_user.id, PrivateChat.user1 == current_user.id)).all()
-    print(data2)
 
     chat, = filter(lambda a: a.id == id, data2)
     messages = db_sess.query(PrivateMessages).filter(PrivateMessages.chat == id).all()
@@ -1091,4 +1087,4 @@ if __name__ == '__main__':
     db_sess = db_session.create_session()
     print(os.getcwd() + '\statiс\profile_photos')
     setup()
-    app.run(port=8080, host='127.0.0.1')
+    app.run(port=8000, host='127.0.0.1')
