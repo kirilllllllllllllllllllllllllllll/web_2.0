6283b98d0b3275271f9a7618cdfb4d86a4989182
Dmitriy <ytude7042@gmail.com>
2025-03-23 19:29:43 +0700

    register

--- forms/addmessage.py
+++ forms/addmessage.py
@@ -6,7 +6,6 @@ from wtforms import SubmitField, StringField
 #>>>>>>> 304bd0b271344315fddd8a1e08198432ab75bd08
 from wtforms.validators import DataRequired
 
-
 class AddMessage(FlaskForm):
     content = StringField('Сообщение', validators=[DataRequired()])
     submit = SubmitField('Отправить')
